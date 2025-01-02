3a3e79a1c42ec666d1756d3f8eb1cc04322dc4e5
Francisco Arencibia Quesada <arencibia.francisco@gmail.com>
2025-01-02 12:30:50 +0100

    Update custom_sso_security_manager.py

--- docker/pythonpath_dev/custom_sso_security_manager.py
+++ docker/pythonpath_dev/custom_sso_security_manager.py
@@ -6,7 +6,7 @@ class CustomSsoSecurityManager(SupersetSecurityManager):
     Custom Security Manager for handling Azure OAuth2 authentication.
     """
 
-    def oauth_user_info(self, provider, response=any):
+    def oauth_user_info(self, provider, response):
         """
         Extracts user information from the OAuth2 response provided by Azure.
 
@@ -16,14 +16,33 @@ class CustomSsoSecurityManager(SupersetSecurityManager):
 
         Returns:
             dict: A dictionary containing user information like name, email,
-                  username, and roles.
+                  username, and mapped roles.
         """
-        logging.debug("Oauth2 provider: {0}.".format(provider))
+        logging.debug(f"Oauth2 provider: {provider}")
 
         if provider == 'azure':
             # Decode and validate the JWT token from Azure
             me = self._decode_and_validate_azure_jwt(response["id_token"])
-            logging.debug("user_data: {0}".format(me))
+            logging.debug(f"User data from Azure: {me}")
+
+            # Extract roles from Azure's JWT token
+            azure_roles = me.get('roles', [])
+            logging.debug(f"Roles from Azure: {azure_roles}")
+
+            # Map Azure roles to Superset roles
+            mapped_roles = []
+            roles_mapping = {
+                "superset_users": ["Gamma"],
+                "superset_admins": ["Admin"],
+            }
+            for azure_role in azure_roles:
+                for superset_role, azure_mapped_roles in roles_mapping.items():
+                    if azure_role in azure_mapped_roles:
+                        mapped_roles.append(superset_role)
+
+            # Ensure roles are unique
+            mapped_roles = list(set(mapped_roles))
+            logging.debug(f"Mapped Superset roles: {mapped_roles}")
 
             # Map the user data fields to a standardized format
             return {
@@ -33,5 +52,5 @@ class CustomSsoSecurityManager(SupersetSecurityManager):
                 'username': me['upn'] if "upn" in me else me['email'],
                 'first_name': me['given_name'],
                 'last_name': me['family_name'],
-                'role_keys': me.get('roles', []),  # Optional roles, default to an empty list
+                'role_keys': mapped_roles,  # Mapped roles for Superset
             }
